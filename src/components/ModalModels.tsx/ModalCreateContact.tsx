6aa443642d430d9a8ca206bead91b38a4b967ea8
facuscrollini <facuscrollinic@gmail.com>
2026-02-04 17:47:12 +0100

    modal crear agenda listo

--- src/components/ModalModels.tsx/ModalCreateContact.tsx
+++ src/components/ModalModels.tsx/ModalCreateContact.tsx
@@ -37,7 +37,7 @@ export const ModalCreateContact = ({ closeModal }: ModalModelType) => {
     }
 
 
-   
+
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         if (!store?.slug) throw new Error("Agenda not valid")
@@ -48,29 +48,31 @@ export const ModalCreateContact = ({ closeModal }: ModalModelType) => {
             const key = field as keyof typeof formData
 
             const value: string = formData[key]
-            setErrors(prev => ({...prev, [key]: value.trim().length < 6}))
+            setErrors(prev => ({ ...prev, [key]: value.trim().length < 6 }))
 
             if (value.trim().length < 6) {
-                if(!error){
+                if (!error) {
                     error = true
                 }
                 setFieldMessage("Please enter at least 6 characters in every field.")
             }
         }
         loadAgenda(store?.slug)
-        
+
         const exist = Array.isArray(store?.contacts) && store.contacts.some((contact) => {
             const contactName = contact.name.replace(" ", "").toLowerCase()
             const formDataName = formData.name.replace(" ", "").toLowerCase()
             return contactName == formDataName
-        })  
+        })
 
         if (exist) {
             setFieldMessage("This contact name already exist, try another one")
+            setErrors({...errors, name : true})
+            return
         }
-        if(!error){
+        if (!error) {
             setFieldMessage("")
-        }else{
+        } else {
             return
         }
 
@@ -86,42 +88,56 @@ export const ModalCreateContact = ({ closeModal }: ModalModelType) => {
 
     return (
         <>
-            <form className="flex flex-col max-w-70 px-5" onSubmit={handleSubmit}>
+            <div className="relative pt-6">
+                <div onClick={() => closeModal()} className="absolute top-0 right-0 p-1 rounded-lg hover:cursor-pointer hover:bg-slate-200">
+                    <i className=" fa-solid fa-xmark"></i>
+                </div>
+                <div className="text-center ">
+                    <span className="text-2xl">Add a new contact</span>
+                </div>
+                <img className="w-20 mx-auto opacity-75 my-3" src="https://marketplace.canva.com/Dz63E/MAF4KJDz63E/1/tl/canva-user-icon-MAF4KJDz63E.png"/>
 
-                {fields.map((field, index) => {
-                    // return (<><FormField disabled={created} key={index} formData={formData} field={field} handleChange={handleChange} /> </>)
-                    const capitalized = field[0].toUpperCase() + field.slice(1)
-                    return (<>
-                        <label htmlFor={field}>{capitalized}</label>
-                        <input className={`mb-4 ${errors[field] ?
-                            "focus:outline-red-300 bg-red-100 text-red-400 border-red-300 "
-                            : "focus:outline-slate-400 bg-slate-10 text-slate-500 border-slate-300 "
+                <form className="flex flex-col max-w-70 py-2" onSubmit={handleSubmit}>
+                    <div className="flex flex-col bg-slate-50 rounded-xl p-5">
+
+                    {fields.map((field, index) => {
+                        // return (<><FormField disabled={created} key={index} formData={formData} field={field} handleChange={handleChange} /> </>)
+                        const capitalized = field[0].toUpperCase() + field.slice(1)
+                        return (<>
+                            <label htmlFor={field}>{capitalized}</label>
+                            <input className={`mb-4 ${errors[field] ?
+                                "focus:outline-red-300 bg-red-100 text-red-400 border-red-300 "
+                                : "focus:outline-slate-400 bg-slate-100 text-slate-500 border-slate-300 "
                             } border-1  h-9 rounded-xl px-3 `}
                             id={field} disabled={created}
                             key={index}
                             name={field}
                             onChange={handleChange} />
-                    </>)
-                })}
-                {
-                    created ?
+                        </>)
+                    })}
+                    {
+                        created ?
                         <p className="text-green-500 whitespace-wrap">{fieldMessage}</p>
                         :
                         <p className="text-red-500">{fieldMessage}</p>
-                }
-                {
-                    created ?
-                        <div>
-                            <InteractiveButton tone="disabled" color="green" text="Created" />
-                            <InteractiveButton tone="normal" buttonType="button" color="slate" text="Close" onClick={() => closeModal()} />
-                        </div>
-                        :
-                        <div>
-                            <InteractiveButton tone="normal" color="green" text="Create" />
-                            <InteractiveButton buttonType="button" tone="normal" color="slate" text="Cancel" onClick={() => closeModal()} />
-                        </div>
-                }
-            </form>
+                    }
+                    </div>
+                    <div className="flex justify-evenly mt-2 ">
+                        {
+                            created ?
+                                <>
+                                    <InteractiveButton tone="disabled" color="green" text="Created" />
+                                    <InteractiveButton tone="normal" buttonType="button" color="slate" text="Close" onClick={() => closeModal()} />
+                                </>
+                                :
+                                <>
+                                    <InteractiveButton tone="normal" color="green" text="Create" />
+                                    <InteractiveButton buttonType="button" tone="normal" color="slate" text="Cancel" onClick={() => closeModal()} />
+                                </>
+                        }
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
