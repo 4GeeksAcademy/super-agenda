93369a1e6f7cba8d7aa3ec5fcf875c58d38e4d03
facuscrollini <facuscrollinic@gmail.com>
2026-02-05 09:59:48 +0100

    ordenando elementos en benefits section en home

--- src/components/Home/BenefitsSection.tsx
+++ src/components/Home/BenefitsSection.tsx
@@ -1,7 +1,16 @@
 export const BenefitsSection = ()=>{
     return(
-        <>
-         <p>Benefits Section</p>
-        </>
+        <div className="px-2 py-25 bg-orange-300  mx-auto mx-auto w-full sm:w-[600px] md:w-[700px] lg:w-[800px] xl:w-[900px] 2xl:w-[1000px]  my-5">
+         <div className="bg-emerald-300 p-2">
+            <div className="bg-red-300 text-center">
+               <h2 className="text-slate-800 text-3xl md:text-4xl 2xl:text-5xl">Why use Super Agenda?</h2>
+            </div>
+            <div className="grid grid-cols-12 gap-2 bg-red-300 mt-3">
+                <div className="col-span-4">Carta 1</div>
+                <div className="col-span-4">Carta 2</div>
+                <div className="col-span-4">Carta 3</div>
+            </div>
+         </div>
+        </div>
     )
 }
