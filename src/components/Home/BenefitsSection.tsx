52ce78db6c49647f1fca2d128130e2f5ce640f4b
facuscrollini <facuscrollinic@gmail.com>
2026-02-05 13:00:07 +0100

    mejora visual en benefits section

--- src/components/Home/BenefitsSection.tsx
+++ src/components/Home/BenefitsSection.tsx
@@ -5,19 +5,87 @@ export const BenefitsSection = () => {
                 <div className="bg-red-300 text-center">
                     <h2 className="text-slate-800 text-3xl md:text-4xl 2xl:text-5xl">Why use Super Agenda?</h2>
                 </div>
-                <div className="grid grid-cols-12 gap-2 bg-red-300 mt-3">
-                    <div className="col-span-4">
-                        <div className="grid grid-cols-12">
-                            <div className="col-span-4 bg-blue-300">s</div>
-                            <div className="col-span-8 bg-violet-300 p-1">
-                                <div className="mb-2 bg-orange-300" >d</div>
-                                <div  className="bg-orange-300" >f</div>
+                <div className="grid grid-cols-12 gap-5 bg-red-300 mt-3">
+
+                    <div className="col-span-6 md:col-span-4  justify-center mt-3">
+                        <div className="bg-slate-100 flex flex-col py-3">
+
+                            <div className="w-50 lg:w-60 grid grid-cols-12 gap-2rounded-xl">
+                                <div className="col-span-12 lg:col-span-4  text-center">
+                                    <div className="p-3 inline-block bg-slate-300 rounded-2xl">
+                                        <i className="py-2 text-4xl text-slate-600 fa-solid fa-user"></i>
+                                    </div>
+                                </div>
+                                <div className="col-span-12 lg:col-span-8 p-1  flex items-center justify-center">
+                                    <div className="mb-2 text-xl lg:text-2xl text-center" >Complete Contact Info</div>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-12">
+                                <div className="lg:col-span-4"></div>
+                                <div className="col-span-12 lg:col-span-8">
+                                    <div className="text-[0.8rem] lg:text-sm text-center" >Every contact includes a name, address, phone, and email.</div>
+
+                                </div>
                             </div>
 
+
                         </div>
+
+
                     </div>
-                    <div className="col-span-4">Carta 2</div>
-                    <div className="col-span-4">Carta 3</div>
+                    <div className="col-span-6 md:col-span-4 flex justify-center mt-3">
+                        <div className="bg-slate-100 flex flex-col py-3">
+
+                            <div className="w-50 lg:w-60 grid grid-cols-12 gap-2rounded-xl">
+                                <div className="col-span-12 lg:col-span-4  text-center">
+                                    <div className="p-3 inline-block bg-slate-300 rounded-2xl">
+                                        <i className="py-2 text-4xl text-slate-600 fa-solid fa-user"></i>
+                                    </div>
+                                </div>
+                                <div className="col-span-12 lg:col-span-8 p-1  flex items-center justify-center">
+                                    <div className="mb-2 text-xl lg:text-2xl  text-center" >Instant Editing</div>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-12">
+                                <div className="lg:col-span-4"></div>
+                                <div className="col-span-12 lg:col-span-8 flex items-center">
+                                    
+                                    <div className="text-[0.8rem] lg:text-sm text-center" > Update or fix any contact in seconds.</div>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                    <div className="col-span-12 md:col-span-4 flex justify-center mt-3">
+
+                        <div className="bg-slate-100 flex flex-col py-3">
+
+                            <div className="w-50 lg:w-60 grid grid-cols-12 gap-2rounded-xl">
+                                <div className="col-span-12 lg:col-span-4  text-center">
+                                    <div className="p-3 inline-block bg-slate-300 rounded-2xl">
+                                        <i className="py-2 text-4xl text-slate-600 fa-solid fa-user"></i>
+                                    </div>
+                                </div>
+                                <div className="col-span-12 lg:col-span-8 p-1  flex items-center justify-center">
+                                    <div className="mb-2 text-xl lg:text-2xl text-center" >Full Control</div>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-12">
+                                <div className="lg:col-span-4"></div>
+                                <div className="col-span-12 lg:col-span-8">
+                                    <div className="text-[0.8rem] lg:text-sm text-center  flex items-center" >Add or remove contacts anytime, keeping your address book clean and organized.</div>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
         </div>
